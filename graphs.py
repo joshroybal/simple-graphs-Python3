71957d9b3691c8e3b6a8e29d2a24210f0b06dffb
Joshua E. Roybal <u4ysev@gmail.com>
2023-02-08 10:36:56 -0600

    Add files via upload
    Added Welch-Powell vertex coloring algorithm.

--- graphs.py
+++ graphs.py
@@ -1,6 +1,9 @@
 from random import choice
 
-labels = [ chr(i) for i in range(65, 91) ]
+labels = [ chr(i) for i in range(97, 122) ]
+
+def ltridx(c):
+    return ord(c) - 97
 
 def vertices(n):
     return labels[:n]
@@ -29,8 +32,24 @@ def adjacency_dict(g):
                 d[i].extend([ k for k in j if k is not i ])
     return d
 
-def ltridx(c):
-    return ord(c) - 65
+def print_adjacency_dict(d):
+    for k in d:
+        print(k + ': ', end='')
+        for v in d[k]:
+            print('[' + v + '|-]--> ', end='')
+        print('|/|')
+
+def degree(node, graph):
+    d = adjacency_dict(graph)
+    return len(d[node])
+
+def degrees(g):
+    d = adjacency_dict(g)
+    return [ degree(k, g) for k in d ]
+
+def ordered_degrees(g):
+    d = adjacency_dict(g)
+    return sorted([ (k, len(d[k])) for k in d ], key = lambda x: x[1], reverse=True)
 
 def adjacency_matrix(g):
     v, e = g
@@ -44,26 +63,60 @@ def adjacency_matrix(g):
         t[j][i] = 1
     return t
 
+def print_adjacency_matrix(m):
+    for row in m:
+        for col in row:
+            print(col, end=' ')
+        print()
+
+def color_queue(g):
+    return [ i[0] for i in ordered_degrees(g) ]
+
+def non_adjacent(v, d):
+    return list(set([ key for key in d ]) - set(d[v]))
+
+def all_non_adjacent(node, t, adjdict):
+    for v in t:
+        if node in adjdict[v]:
+            return False
+    return True
+
+def paint(g):
+    colors = ['red','orange','yellow','green','blue','indigo','violet']
+    q = color_queue(g)
+    d = adjacency_dict(g)
+    nodes = g[0]
+    color_dict = {}
+    while len(q) > 0:
+        node = q[0]
+        t = non_adjacent(node, d)
+        c = choice(colors)
+        colors.remove(c)
+        for v in t:
+            if v in q and all_non_adjacent(v, t, d) == True:
+                color_dict[v] = c
+                q.remove(v)
+                if len(q) == 0: break
+    return color_dict
+
 def dot_dump(g):
-    colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet' ]
-    #c = ' node [fillcolor=green] '
     nodes = g[0]
     edges = g[1]
     idx = 0
-    fillcolors = '\n'
     for node in nodes:
-        fillcolors += '"' + node + '" [fillcolor=' + colors[idx] + ']\n'
         idx = (idx + 1) % 7
-    print(fillcolors)
-    s = 'graph { layout=circo; node [ shape=circle height=0 width=0 margin=0 \
-style=filled ] ratio=0.618; pad=1.0;'
+    s = 'graph { layout=circo; node [ shape=circle margin=0 height=0 width=0 style=filled ]'
     idx = 0
     for edge in edges:
         idx = (idx + 1) % 7
         s += ' ' + str(edge[0]) + ' -- ' + str(edge[1]) + ' '
-    s += fillcolors
+    s += '\n'
+    fillcolors = paint(g)
+    for key in fillcolors:
+        s += key + ' [fillcolor=' + fillcolors[key] + ']\n'
     s += ' }'
     print(s)
     f = open('graph.gv', 'w')
     f.write(s)
     f.close()
+    print('graph data dumped to file graph.gv')
