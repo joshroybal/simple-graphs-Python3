5d8cb82931db139628f38e70969c51fca93dadd8
Joshua E. Roybal <u4ysev@gmail.com>
2023-03-23 12:29:05 -0500

    Add files via upload
    Rewrite. Now uses namedtuple for Edge. Added methods to add and remove vertices and edges.

--- debug.py
+++ debug.py
@@ -1,49 +1,55 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys, os, random
-import graphs
-
-def main():
-    # random graph
-    print('random graph')
-    g = graphs.random_graph()
-    print(g)
-    if g.density() <= .5:
-        graphs.print_adjacency_dict(g)
-    if g.density() >= .5:
-        graphs.print_adjacency_matrix(g)
-    print('size = {:d}, order = {:d}, density = {:f}'.format(g.size(), g.order(), g.density()))
-    graphs.dot_point(g, 'random.svg')
-    # complete graph
-    print('complete graph')
-    n = random.randint(2, 12)
-    g = graphs.complete_graph(n)
-    print(g)
-    graphs.print_adjacency_matrix(g)
-    print('size = {:d}, order = {:d}, density = {:f}'.format(g.size(), g.order(), g.density()))
-    graphs.dot_point(g, 'complete.svg')
-    # regular graph
-    print('regular graph')
-    n = random.randint(3, 26)
-    g = graphs.regular_graph(n)
-    print(g)
-    if g.density() <= .5:
-        graphs.print_adjacency_dict(g)
-    if g.density() >= .5:
-        graphs.print_adjacency_matrix(g)
-    print('size = {:d}, order = {:d}, density = {:f}'.format(g.size(), g.order(), g.density()))
-    graphs.dot_point(g, 'regular.svg')
-    # sparse graph
-    print('sparse graph')
-    g = graphs.sparse_graph()
-    print(g)
-    graphs.print_adjacency_dict(g)
-    print('size = {:d}, order = {:d}, density = {:f}'.format(g.size(), g.order(), g.density()))
-    graphs.dot_point(g, 'sparse.svg')
+import sys
+from graphs import Graph, random_graph, dotpoint, dotcolor, welsh_powell,\
+ graph_report, complete_edges
+from time import time
 
 if __name__ == '__main__':
+    # g = complete_graph(5)
+    # print(g)
+    # print('size =', g.size())
+    # print('order =', g.order())
+    # g.printnodes()
+    # g.printedges()
+    # g.printadjlis()
+    # print('adjacent(G, b, c) =', g.adjacent('b', 'c'))
+    # print('antiedge(G, b, c) =', g.antiedge('b', 'c'))
+    # print('neighbors(G, d) =', g.neighbors('d'))
+    # print('antiedges(G, d) =', g.antiedges('d'))
+    # print('writing 1.svg')
+    # dotcolor(g, welsh_powell(g), '1.svg')
+
+    # print('add node f')
+    # g.addnode('f')
+    # print(g)
+    # print('writing 2.svg')
+    # dotcolor(g, welsh_powell(g), '2.svg')
+
+    # print('remove node d')
+    # g.removenode('d')
+    # print(g)
+    # print('writing 3.svg')
+    # dotcolor(g, welsh_powell(g), '3.svg')
+
+    # print('add edge {f,b}')
+    # g.addedge('f', 'b')
+    # print(g)
+    # print('writing 4.svg')
+    # dotcolor(g, welsh_powell(g), '4.svg')
+
+    # print('remove edge {c,b}')
+    # g.remove_edge('c', 'b')
+    # print(g)
+    # print('writing 5.svg')
+    # dotcolor(g, welsh_powell(g), '5.svg')
+
+    g = random_graph()
+    graph_report(g)
+    #g = Graph([1,2,3,4,5], complete_edges([1,2,3,4,5]))
+    #graph_report(g)
     try:
-        main()
+        dotcolor(g, welsh_powell(g), 'random.svg')
     except Exception as error:
         sys.stderr.write('%s\n' % error)
-        sys.exit(1)
+        dotpoint(g, 'random.svg')
--- graphs.py
+++ graphs.py
@@ -1,138 +1,185 @@
-import math, os, subprocess, graphviz
-from random import choice, randint, sample
-from randnorm import randint_normal, randint_lognormal
+import os, subprocess, graphviz
+from random import randint, sample
+from collections import namedtuple
 
-labels = [ chr(i) for i in range(ord('a'), ord('a') + 26) ]
+letters = [ chr(i) for i in range(97,123) ]
 
-class Edge:
-    def __init__(self, v1, v2):
-        self.v1, self.v2 = v1, v2
-    def __init__(self, t):  # init with tuple
-        self.v1, self.v2 = t
-    def __str__(self):
-        return '{' + self.v1 + ',' + self.v2 + '}'
+def ltridx(ltr):
+    return ord(ltr) - 97
+
+Edge = namedtuple('Edge', ['x', 'y'])
 
 class Graph:
-    def __init__(self, nodes=[], edges=[]):
-        self.nodes = nodes
-        self.edges = edges
+    """Represents an undirected simple graph.
+    attributes: adjlis
+    """
+    def __init__(self, nodes, edges):
+        self.adjlis = { node: set() for node in nodes }
+        for e in edges:
+            self.adjlis[e.x].add(e.y)
+            self.adjlis[e.y].add(e.x)
+
+    def nodes(self):
+        return [ v for v in self.adjlis ]
+
+    # There has got to be a better way.
+    def edges(self):
+        edgeset = set()
+        for x in self.adjlis:
+            for y in self.adjlis[x]:
+                edgeset.add(Edge(*sorted([x, y])))
+        return edgeset
 
     def size(self):
-        return len(self.edges)
+        return sum( [ len(x) for x in self.adjlis.values() ] ) // 2
 
     def order(self):
-        return len(self.nodes)
+        return len(self.adjlis)
 
     def density(self):
-        m = len(self.edges)
-        n = len(self.nodes)
-        return 2*m / (n*(n-1))
+        m = self.size()
+        n = self.order()
+        return 2 * m / (n * (n - 1))
 
     def adjacent(self, x, y):
-        return (x, y) in self.edges or (y, x) in self.edges
+        """Tests whether there is an edge from vertex x to the vertex y."""
+        return y in self.adjlis[x]
+
+    def antiedge(self, x, y):
+        """Tests whether there is not an edge from vertex x to the vertex y."""
+        return not self.adjacent(x, y)
 
     def neighbors(self, x):
-        return [ y for y in self.nodes if self.adjacent(x, y) ]
+        """Lists all vertices y such that there is an edge from the vertex x to the vertex y."""
+        return self.adjlis[x]
 
-    def adjacency_dict(self):
-        return dict( [ (v, self.neighbors(v)) for v in self.nodes ] )
+    def antiedges(self, x):
+        """Lists all vertices y such that there is not an edge from the vertex x to the vertex y."""
+        return set(self.nodes()).difference(self.adjlis[x])
 
-    def adjacency_matrix(self):
-        t = [ self.order() * [ 0 ] for i in range(self.order()) ]
-        for edge in self.edges:
-            c1, c2 = edge
-            i = ltridx(c1)
-            j = ltridx(c2)
+    def addnode(self, x):
+        """Adds the vertex x, if it is not there."""
+        if x not in self.adjlis:
+            self.adjlis[x] = set()
+
+    def removenode(self, x):
+        """Remove the vertex x, if it is there."""
+        self.adjlis.pop(x)
+        for node in self.adjlis:
+            if x in self.adjlis[node]:
+                self.adjlis[node].remove(x)
+
+    def addedge(self, x, y):
+        """Adds the edge from the vertex x to the vertex y if it is not there."""
+        if self.antiedge(x, y):
+            self.adjlis[x].add(y)
+            self.adjlis[y].add(x)
+
+    def remove_edge(self, x, y):
+        """Removes the edge from the vertex x to the vertex y if it is there."""
+        if self.adjacent(x, y):
+            self.adjlis[x].remove(y)
+            self.adjlis[y].remove(x)
+
+    def degree(self, x):
+        return len(self.adjlis[x])
+
+    def degrees(self):
+        d = {}
+        for v in self.adjlis:
+            d[v] = self.degree(v)
+        return d
+
+    def adjmat(self):
+        n = self.order()
+        t = [ [ 0 ] * n for i in range(n) ]
+        for edge in self.edges():
+            if type(edge.x) is str and type(edge.y) is str:
+                i, j = ltridx(edge.x), ltridx(edge.y)
+            else:
+                i, j = edge.x - 1, edge.y - 1
             t[i][j] = 1
             t[j][i] = 1
         return t
 
-    def degree_dict(self):
-        adjlis = self.adjacency_dict()
-        return dict([ (k,len(adjlis[k])) for k in adjlis ])
+    def printnodes(self):
+        print('{' + ','.join(self.nodes()) + '}')
+
+    def printedges(self):
+        print('{'+','.join(['{'+','.join(e)+'}' for e in sorted(self.edges())])+'}')
+
+    def printadjlis(self):
+        for v in self.adjlis:
+            print(str(v) + ': ' + ','.join(map(str, self.adjlis[v])))
+
+    def printadjmat(self):
+        print('\n'.join(' '.join(str(j) for j in i) for i in self.adjmat()))
 
     def __str__(self):
-        return '(V,E) = ({'+','.join([v for v in self.nodes]) + '},\
-{'+','.join(['{'+','.join([v for v in e])+'}' for e in self.edges])+'})'
+        v = '{' + ','.join(map(str, self.nodes())) + '}'
+        e = '{'+','.join(['{'+','.join(map(str, e))+'}' for e in sorted(self.edges())])+'}'
+        return '(V,E) = (' + v + ',' + e + ')'
 
-def ltridx(c):
-    return ord(c) - 97
-
-def idxltr(i):
-    return chr(97 + i)
+def complete_nodes(n):
+    return letters[:n]
 
 def complete_edges(v):
-    n = len(v)
-    #t = [ Edge(map(idxltr, (i,j))) for i in range(n) for j in range(i+1,n) ]
-    #for e in t:
-    #    print(e)
-    return [ tuple(map(idxltr, (i,j))) for i in range(n) for j in range(i+1,n) ]
+    return [ Edge(x,y) for x in v[:-1] for y in v[v.index(x) + 1:] ]
 
 def complete_graph(n):
-    return Graph(labels[:n], complete_edges(labels[:n]))
+    v = complete_nodes(n)
+    e = complete_edges(v)
+    return Graph(v, e)
 
-def regular_edges(nodes):
-    n = len(nodes)
-    return sorted( [ tuple(sorted( [ nodes[i],nodes[(i+1)%n ] ] ) ) for i in range(n) ] )
+def random_graph(n=randint(2, 26)):
+    maxm = (n * (n - 1)) // 2
+    m = randint(1, maxm)
+    v = letters[:n]
+    e = sample(complete_edges(v), m)
+    return Graph(v, e)
 
-def regular_graph(n):
-    return Graph(labels[:n], regular_edges(labels[:n]))
+def dotpoint(g, filename):
+    dot = graphviz.Graph(os.path.basename(filename))
+    dot.attr('node', shape='point')
+    for node in g.nodes():
+        dot.node(node)
+    for edge in g.edges():
+        dot.edge(edge.x, edge.y)
+    output = dot.render(outfile=filename,engine='circo',cleanup=False)
+    print(output)
 
-def random_edges(v):
-    n = len(v)
-    m = randint(1, (n * (n - 1)) / 2)
-    return sorted(sample(complete_edges(v), m))
+def dotcolor(g, colors, filename):
+    c = len( { v for v in colors.values() } )
+    m = g.size()
+    n = g.order()
+    d = g.density()
+    # label = 'colors = {:d}, size = {:d}, order = {:d}, density = {:.3f}'.format(c, m, n, d)
+    dot = graphviz.Graph(os.path.basename(filename))
+    # dot.attr(ratio='0.618')
+    # dot.attr(pad='1')
+    # dot.attr('graph',label=label)
+    dot.attr('node', shape='circle',style='filled',height='0',width='0',margin='0')
+    for node in colors:
+        dot.node(node,fillcolor=colors[node])
+    for edge in g.edges():
+        dot.edge(edge.x, edge.y)
+    output = dot.render(outfile=filename,engine='circo',cleanup=True)
+    print(output)
 
-def random_graph():
-    n = randint(2, 26)
-    return Graph(labels[:n], random_edges(labels[:n]))
-
-# sort of log normal distributed sparse graphs...
-def sparse_graph():
-    n = randint(4, 26)
-    maximum = n * (n - 1) / 2
-    lo = 1
-    hi = math.ceil(maximum/2) - 1
-    m = randint_normal(lo, hi)
-    return Graph(labels[:n], sorted(sample(complete_edges(labels[:n]), m)))
-
-def print_adjacency_dict(g):
-    d = g.adjacency_dict()
-    for k in d:
-        print(k + ': ', end='')
-        for v in d[k]:
-            print(v, end='')
-            if v != d[k][-1]:
-                print(',', end='')
-        print()
-
-def degree(node, g):
-    return g.degree_dict()[node]
 
 def descending_order(d):
     return [ k for k,v in sorted(d.items(), key = lambda x: x[1], reverse=True) ]
 
-def print_adjacency_matrix(g):
-    m = g.adjacency_matrix()
-    for row in m:
-        for col in row:
-            print(col, end=' ')
-        print()
-
 # Welsh-Powell vertex coloring algorithm.
-def colornodes(g):
+def welsh_powell(g):
     colors = ['red','orange','yellow','green','blue','indigo','violet']
-    c0 = len(colors)
-    d = g.adjacency_dict()
-    q = descending_order(g.degree_dict())
+    d = g.adjlis
+    # q = descending_order(g.degrees())
+    q = [ k for k,v in sorted(g.degrees().items(), key = lambda x: x[1], reverse=True) if v > 0 ]
     color_dict = {}
     while len(q) > 0 and len(colors) > 0:
-        #print('nodes ordered by degree')
-        #print(q)
         node = q[0]
         t = [ v for v in q if v not in d[node] ]
-        #print('nodes not adjacent to', node)
-        #print(t)
         c = colors.pop(0)
         atc = []    # assigned this color
         for v in t:
@@ -143,34 +190,23 @@ def colornodes(g):
             q.remove(v)
             if len(q) == 0:
                 break
-        #print('nodes assigned color', c, atc)
-        #print()
     if len(q) > 0:
         raise ValueError("not enough colors")
-    #print('no. of colors assigned:', c0 - len(colors))
     return color_dict
 
-def dot_point(g, filename):
-    dot = graphviz.Graph(os.path.basename(filename))
-    dot.attr('graph',label='size = {:d}, order = {:d}, density = {:f}'.format(g.size(), g.order(), g.density()))
-    dot.attr('node', shape='point')
-    for node in g.nodes:
-        dot.node(node)
-    for edge in g.edges:
-        dot.edge(edge[0], edge[1])
-    output = dot.render(outfile=filename,engine='circo',cleanup=True)
-    print(output)
-
-def dot_color(g, colors, filename):
-    nc = len({v for k, v in colors.items()})
-    dot = graphviz.Graph(os.path.basename(filename))
-    # dot.attr(ratio='0.618')
-    # dot.attr(pad='1')
-    dot.attr('graph',label='colors = {:d}, size = {:d}, order = {:d}, density = {:f}'.format(nc, g.size(), g.order(), g.density()))
-    dot.attr('node', shape='circle',style='filled',height='0',width='0',margin='0')
-    for node in colors:
-        dot.node(node,fillcolor=colors[node])
-    for edge in g.edges:
-        dot.edge(edge[0], edge[1])
-    output = dot.render(outfile=filename,engine='circo',cleanup=True)
-    print(output)
+def graph_report(g):
+    # processing
+    size = g.size()
+    order = g.order()
+    density = g.density()
+    # output
+    print(g)
+    if density <= .5:
+        g.printadjlis()
+    if density >= .5:
+        g.printadjmat()
+    # for v, d in g.degrees().items():
+        # print('degree({:s}) = {:d}'.format(str(v), d))
+    # for v in g.nodes():
+        # print(str(v) + ': ', g.antiedges(v))
+    print('size = {:d}, order = {:d}, density = {:f}'.format(size, order, density))
